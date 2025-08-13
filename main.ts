52221394e42e97cdeda54081e5aa4717f76784b7
WallySa7 <dooody05314251@gmail.com>
2025-08-13 18:15:27 +0300

    added ribbon icon

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
-import { Plugin, MarkdownPostProcessorContext } from "obsidian";
+import {
+	Plugin,
+	MarkdownPostProcessorContext,
+	Notice,
+	MarkdownView,
+} from "obsidian";
 import { AbyatModal } from "./src/modal";
 import { AbyatPoem } from "./src/types";
 import { AbyatRenderer } from "./src/renderer";
@@ -23,11 +28,28 @@ export default class AbyatPlugin extends Plugin {
 		this.renderer = new AbyatRenderer(this.app);
 		this.parser = new AbyatParser();
 
+		// Add ribbon icon for quick access
+		this.registerRibbonIcons();
+
 		// Register plugin commands
 		this.registerCommands();
 
 		// Register markdown processors
 		this.registerMarkdownProcessors();
+	}
+
+	/**
+	 * Register ribbon icons for quick access
+	 */
+	private registerRibbonIcons(): void {
+		// Main library icon
+		this.addRibbonIcon(
+			"scroll",
+			"إنشاء قصيدة عربية جديدة",
+			(evt: MouseEvent) => {
+				this.openPoemModal();
+			}
+		);
 	}
 
 	/**
@@ -45,6 +67,43 @@ export default class AbyatPlugin extends Plugin {
 				modal.open();
 			},
 		});
+	}
+
+	/**
+	 * Open the poem creation modal
+	 */
+	private openPoemModal(): void {
+		const activeFile = this.app.workspace.getActiveFile();
+		const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+
+		if (!activeFile) {
+			new Notice("الرجاء فتح ملاحظة أولاً");
+			return;
+		}
+
+		if (!activeView || activeView.getMode() !== "source") {
+			new Notice("الرجاء التبديل إلى وضع التحرير أولاً");
+			return;
+		}
+
+		const modal = new AbyatModal(this.app, (poem: AbyatPoem) => {
+			this.insertPoemInActiveEditor(poem);
+		});
+		modal.open();
+	}
+
+	/**
+	 * Insert poem in the currently active editor
+	 */
+	private insertPoemInActiveEditor(poem: AbyatPoem): void {
+		const activeLeaf = this.app.workspace.getLeaf();
+
+		// Check if we have an editor view
+		const view = activeLeaf.view as MarkdownView;
+		if (view?.editor) {
+			const poemMarkdown = this.parser.generatePoemMarkdown(poem);
+			view.editor.replaceSelection(poemMarkdown);
+		}
 	}
 
 	/**
